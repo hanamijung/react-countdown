81a09f9113908444051b250483364bea44c9e9ea
hanamijung <hanamispkz@gmail.com>
2019-11-03 03:44:44 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
         </div>
 
         <h3 className="title">Christmas Eve is coming soon (Midnight of 23rd to 24th Dec, UTC time):</h3>
-        <Countdown date={`${year}-12-24T00:00:00`} />
+        <Countdown date={`${year}-10-17T00:00:00`} />
       </div>
     );
   }
